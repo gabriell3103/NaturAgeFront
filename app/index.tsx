fb6e2a2764c5de256e47bf44c1e90e6ff77e794b
gabriell3103 <g5819853@gmail.com>
2025-05-01 16:38:38 -0300

    Remoção do NativeWind e criação da tela de login

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import LoginScreen from '../components/LoginScreen';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <LoginScreen />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});

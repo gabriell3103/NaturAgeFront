ba3c2648366dc48c4a34b7ea4c400f8985165b75
gabriell3103 <g5819853@gmail.com>
2025-05-02 01:24:28 -0300

    Correção de arquivos

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,65 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import LoginScreen from '../components/LoginScreen';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from 'react-native';
+import { router } from 'expo-router';
 
-export default function Index() {
+export default function LoginScreen(): JSX.Element {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   return (
     <View style={styles.container}>
-      <LoginScreen />
+      <Image source={require('../assets/images/Logo.png')} style={styles.logo} />
+
+      <View style={styles.card}>
+        <Text style={styles.welcome}>Bem vindo(a)</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor="#666"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          placeholderTextColor="#666"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        />
+
+        <TouchableOpacity
+          onPress={() => router.push('/home')}
+          style={styles.primaryButton}
+        >
+          <Text style={styles.primaryButtonText}>Continuar</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.registerText}>
+          Caso não possua uma conta{' '}
+          <Text style={styles.link}>registre-se</Text>
+        </Text>
+
+        <TouchableOpacity style={styles.googleButton}>
+          <Image
+            source={require('../assets/images/Logo-Google.png')}
+            style={styles.googleIcon}
+          />
+          <Text style={styles.googleButtonText}>Continuar com Google</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text>© 2024 Conheço uma Ponte. All rights reserved.</Text>
     </View>
   );
 }
@@ -13,5 +67,76 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  logo: {
+    width: 220,
+    height: 220,
+    resizeMode: 'contain',
+    marginBottom: 10,
+  },
+  card: {
+    width: '100%',
+    backgroundColor: '#F5F5F5',
+    padding: 20,
+    borderRadius: 12,
+    alignItems: 'center',
+    elevation: 2,
+  },
+  welcome: {
+    fontSize: 24,
+    marginBottom: 15,
+    color: '#222',
+  },
+  input: {
+    width: '100%',
+    backgroundColor: '#DDD',
+    padding: 12,
+    borderRadius: 8,
+    marginBottom: 10,
+    fontSize: 14,
+  },
+  primaryButton: {
+    width: '100%',
+    backgroundColor: '#49AD78',
+    paddingVertical: 12,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  primaryButtonText: {
+    color: '#060609',
+    fontWeight: 'bold',
+  },
+  registerText: {
+    fontSize: 12,
+    color: '#333',
+    marginTop: 12,
+  },
+  link: {
+    color: '#49AD78',
+    textDecorationLine: 'underline',
+  },
+  googleButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#E0E0E0',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+    marginTop: 15,
+    width: '100%',
+    justifyContent: 'center',
+  },
+  googleIcon: {
+    width: 18,
+    height: 18,
+    marginRight: 8,
+  },
+  googleButtonText: {
+    fontWeight: '600',
   },
 });
--- components/RegisterScreen.tsx
+++ components/RegisterScreen.tsx
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 
 export default function LoginScreen() {
+  const [nome, setNome] = useState('');
+  const [idade, setIdade] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   return (
     <View style={styles.container}>
@@ -14,11 +17,29 @@ export default function LoginScreen() {
 
         <TextInput
           style={styles.input}
+          placeholder="Nome"
+          placeholderTextColor="#666"
+          value={nome}
+          onChangeText={setNome}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Idade"
+          placeholderTextColor="#666"
+          secureTextEntry
+          value={idade}
+          onChangeText={setIdade}
+        />
+
+        <TextInput
+          style={styles.input}
           placeholder="Email"
           placeholderTextColor="#666"
+          secureTextEntry
           value={email}
           onChangeText={setEmail}
         />
+
         <TextInput
           style={styles.input}
           placeholder="Senha"
@@ -28,23 +49,20 @@ export default function LoginScreen() {
           onChangeText={setPassword}
         />
 
+        <TextInput
+          style={styles.input}
+          placeholder="Confirmar senha"
+          placeholderTextColor="#666"
+          secureTextEntry
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        />
+
         <TouchableOpacity style={styles.primaryButton}>
           <Text style={styles.primaryButtonText}>Continuar</Text>
         </TouchableOpacity>
-
-        <Text style={styles.registerText}>
-          Caso não possua uma conta{' '}
-          <Text style={styles.link}>registre-se</Text>
-        </Text>
-
-        <TouchableOpacity style={styles.googleButton}>
-          <Image
-            source={require("../assets/images/Logo-Google.png")}
-            style={styles.googleIcon}
-          />
-          <Text style={styles.googleButtonText}>Continuar com Google</Text>
-        </TouchableOpacity>
       </View>
+      <Text>&copy; 2024 Conheço uma Ponte. All rights reserved.</Text>
     </View>
   );
 }
@@ -63,12 +81,6 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
     marginBottom: 10,
   },
-  title: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    color: '#2E9E5F',
-    marginBottom: 20,
-  },
   card: {
     width: '100%',
     backgroundColor: '#F5F5F5',
@@ -78,7 +90,7 @@ const styles = StyleSheet.create({
     elevation: 2,
   },
   welcome: {
-    fontSize: 16,
+    fontSize: 24,
     marginBottom: 15,
     color: '#222',
   },
@@ -92,42 +104,14 @@ const styles = StyleSheet.create({
   },
   primaryButton: {
     width: '100%',
-    backgroundColor: '#4CAF50',
+    backgroundColor: '#49AD78',
     paddingVertical: 12,
     borderRadius: 8,
     alignItems: 'center',
     marginTop: 5,
   },
   primaryButtonText: {
-    color: '#fff',
+    color: '#060609',
     fontWeight: 'bold',
-  },
-  registerText: {
-    fontSize: 12,
-    color: '#333',
-    marginTop: 12,
-  },
-  link: {
-    color: '#2E9E5F',
-    textDecorationLine: 'underline',
-  },
-  googleButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#E0E0E0',
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-    borderRadius: 8,
-    marginTop: 15,
-    width: '100%',
-    justifyContent: 'center',
-  },
-  googleIcon: {
-    width: 18,
-    height: 18,
-    marginRight: 8,
-  },
-  googleButtonText: {
-    fontWeight: '600',
   },
 });
